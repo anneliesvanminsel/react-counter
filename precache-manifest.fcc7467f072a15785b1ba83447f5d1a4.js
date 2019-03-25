452ec7298b7d0fb0da7e4b8c63fb27e9271c8f3b
Annelies Van Minsel <annelies.van.minsel@student.ehb.be>
2019-03-25 16:54:52 +0100

    Updates

---
+++ precache-manifest.fcc7467f072a15785b1ba83447f5d1a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "96525926c3e3b36425eac890b03af593",
+    "url": "/wieni-counter/static/media/increment.96525926.svg"
+  },
+  {
+    "revision": "0bf66351c206046f8265b323cfaa9afa",
+    "url": "/wieni-counter/static/media/decrement.0bf66351.svg"
+  },
+  {
+    "revision": "e910aab9a37e7172a3ba",
+    "url": "/wieni-counter/static/js/runtime~main.ed1bd51f.js"
+  },
+  {
+    "revision": "101446759424c0a183ef",
+    "url": "/wieni-counter/static/js/main.8052daa4.chunk.js"
+  },
+  {
+    "revision": "b88c4cace6729c7c0c26",
+    "url": "/wieni-counter/static/js/2.2563b717.chunk.js"
+  },
+  {
+    "revision": "101446759424c0a183ef",
+    "url": "/wieni-counter/static/css/main.1be6793a.chunk.css"
+  },
+  {
+    "revision": "8cf32e7b5df49bd9dfee7895db9fb690",
+    "url": "/wieni-counter/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/wieni-counter/precache-manifest.fcc7467f072a15785b1ba83447f5d1a4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/wieni-counter/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
