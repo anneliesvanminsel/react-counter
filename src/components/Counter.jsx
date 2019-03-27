8155c5229b64928cf4fc51163e2373030900253e
Annelies Van Minsel <annelies.van.minsel@student.ehb.be>
2019-03-27 13:38:58 +0100

    Added selectbox, added useEffect, added initial value

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,32 +1,67 @@
-import React from 'react';
-import { useCounter } from 'hooks/useCounter';
-import { useLocalStorage } from 'hooks/useLocalStorage';
-import { ReactComponent as Increment } from 'icons/increment.svg';
-import { ReactComponent as Decrement } from 'icons/decrement.svg';
+import React, {useEffect} from 'react';
+import {createStore} from 'redux';
+import {useCounter} from 'hooks/useCounter';
+import {useLocalStorage} from 'hooks/useLocalStorage';
+import {ReactComponent as Increment} from 'icons/increment.svg';
+import {ReactComponent as Decrement} from 'icons/decrement.svg';
 
 const Counter = () => {
     const initialCounter = 10;
     const [localStorage, setLocalStorage] = useLocalStorage('counter', initialCounter);
     const {counter, increment, decrement} = useCounter(localStorage);
 
+    let initialValue = 10;
+
     const handleDecrement = () => {
-        decrement();
-        setLocalStorage(counter - 1);
+        if (!(counter - initialValue < 0)) {
+            store.dispatch({type: "DECREMENT"})
+        }
     };
 
     const handleIncrement = () => {
-        increment();
-        setLocalStorage(counter + 1);
+
+        store.dispatch({type: "INCREMENT"})
+
     };
+
+    const count = (ctr = counter, action) => {
+        switch (action.type) {
+            case "INCREMENT":
+                increment(initialValue);
+                return ctr + initialValue;
+            case "DECREMENT":
+                decrement(initialValue);
+                return ctr - initialValue;
+            default:
+                return counter;
+        }
+    };
+
+    const handleOnChangeInput = (e) => {
+        console.log(e.target)
+    };
+
+    useEffect(() => setLocalStorage(counter), [counter]);
+    let store = createStore(count);
+    store.subscribe(() => console.log(store.getState()))
 
     return (
         <div className="block">
+            <form>
+                <fieldset>
+                    <select className="select" onChange={handleOnChangeInput}>
+                        <option>1</option>
+                        <option>5</option>
+                        <option>10</option>
+                    </select>
+                </fieldset>
+            </form>
             <button className="button button--decrement" onClick={handleDecrement}>
-                <Decrement />
+                <Decrement/>
             </button>
             <div className="block__text"> {counter} </div>
             <button className="button button--increment" onClick={handleIncrement}>
-                <Increment />
+                <Increment/>
             </button>
         </div>
     );
